b4c8b52ddb05b0b39dddcace65f2954f9cf8bf78
kiran <kgudida@gmail.com>
2025-10-25 15:45:32 +0800

    terraform vpc

--- roboshop-dev-infra/00-vpc/main.tf
+++ roboshop-dev-infra/00-vpc/main.tf
@@ -1,7 +1,9 @@
 module "vpc" {
     #source = "git::https://github.com/daws-86s/terraform-aws-vpc.git?ref=main"
     # VPC
-    source = "git::https://github.com/kirangudida/terraform-devops/terraform-aws-vpc.git?ref=main"
+    #source = "git::https://github.com/kirangudida/terraform-devops/terraform-aws-vpc.git?ref=main"
+    #source = "git::https://github.com/kirangudida/terraform-devops.git?ref=main//terraform-aws-vpc"
+    source = "../../terraform-aws-vpc"
     vpc_cidr = var.vpc_cidr
     project_name = var.project_name
     environment = var.environment
--- roboshop-dev-infra/00-vpc/provider.tf
+++ roboshop-dev-infra/00-vpc/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "remote-state-roboshop-dev"
+    bucket = "remote-state-roboshop-dev-kiran"
     key    = "roboshop-dev-vpc"
     region = "us-east-1"
     use_lockfile = true
